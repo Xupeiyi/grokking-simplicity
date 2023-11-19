476da3060196998aba89a3b8856f7de00409774e
Peiyi Xu <xu_py311@outlook.com>
2023-11-19 10:39:07 -0500

    finished chapter 4

--- cp4.js
+++ cp4.js
@@ -1,22 +1,37 @@
 let shopping_cart = [];
-let shopping_car_total = 0;
+let shopping_cart_total = 0;
 
-function add_item_to_cart(name, price) {
-    shopping_cart.push({
+function add_item(cart, name, price) {
+    let new_cart = cart.slice();
+    new_cart.push({
         name: name,
         price: price
     });
+    return new_cart;
+}
+
+function add_item_to_cart(name, price) {
+    shopping_cart = add_item(shopping_cart, name, price)
     calc_cart_total();
-};
+}
+
+function calc_total(cart) {
+    let total = 0;
+    for (let item of cart) {
+        total += item.price;
+    }
+    return total;
+}
 
 function calc_cart_total() {
-    shopping_cart_total = 0;
-    for (let item of shopping_cart) {
-        shopping_car_total += item.price;
-    }
+    shopping_cart_total = calc_total(shopping_cart);
     set_cart_total_dom();
     update_shipping_icons();
     update_tax_dom();
+}
+
+function gets_free_shipping(item_price, total) {
+    return item_price + total >= 20
 }
 
 // show an icon next to the buy button 
@@ -25,7 +40,7 @@ function update_shipping_icons() {
     let buy_buttons = get_buy_buttons_dom();
     for (let button of buy_buttons) {
         let item = button.item;
-        if (item.price + shopping_cart_total >= 20) {
+        if (gets_free_shipping(item.price, shopping_cart_total)) {
             button.show_free_shipping_icon();
         }
         else {
@@ -35,6 +50,10 @@ function update_shipping_icons() {
 }
 
 function update_tax_dom() {
-    set_tax_dom(shopping_cart_total * 0.10);
+    let cart_total = calc_tax(shopping_cart_total);
+    set_tax_dom(cal);
 }
 
+function calc_tax(amount) {
+    return amount * 0.10;
+}
--- cp4.md
+++ cp4.md
@@ -1,2 +1,7 @@
 Implicit inputs and outputs make a function an action.
 Convert an action to calculation: Replace implicit inputs with arguments, and replace the implicit output with return values
+
+Steps for extracting a calculation:
+1. select and extract the code
+2. identify implicit inputs and outputs
+3. convert inputs to arguments and outputs to return values
